bfb3db3ca210e6a7687610334c4f46c2ff53c4ec
HanaEldemery <hanaeldemery2002@gmail.com>
2024-05-06 16:15:06 +0300

    new

--- project/src/App.js
+++ project/src/App.js
@@ -1,23 +1,61 @@
-import logo from "./logo.svg";
+/*import logo from "./logo.svg";*/
 import "./App.css";
+import Login from "./pages/Login";
+import DonorReg from "./pages/DonorReg";
+import Roleselection from "./pages/roleselection";
+import Doctorrole from "./pages/doctorrole";
+import Donorrole from "./pages/donorrole";
+import Teacherrole from "./pages/teacherrole";
+import AdminOrgReq from "./pages/AdminOrgReq";
+import AdminDonReq from "./pages/AdminDonReq";
+import Adminchangepass from "./pages/Adminchangepass";
+import OrgReg from "./pages/OrgReg";
+import Teacherprobono from "./pages/teacherprobono";
+import Home from "./pages/Home";
+import DonationRequests from "./pages/DonationRequests";
+import ToysRequests from "./pages/ToysRequests";
+import FoodRequests from "./pages/FoodRequests";
+import MedicalSuppliesRequests from "./pages/MedicalSuppliesRequests";
+import BloodRequests from "./pages/BloodRequests";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/donationRequests"
+            element={<DonationRequests />}
+          />
+          <Route exact path="/toysRequests" element={<ToysRequests />} />
+          <Route exact path="/foodRequests" element={<FoodRequests />} />
+          <Route
+            exact
+            path="/medicalSuppliesRequests"
+            element={<MedicalSuppliesRequests />}
+          />
+          <Route exact path="/bloodRequests" element={<BloodRequests />} />
+        </Routes>
+      </Router>
+      <Router>
+        <Routes>
+          <Route path="/adminlogin" exact element={<Login />} />
+          <Route path="/don" exact element={<DonorReg />} />
+          <Route path="/role" exact element={<Roleselection />} />
+          <Route path="/doctorrole" exact element={<Doctorrole />} />
+          <Route path="/DonorReg" exact element={<DonorReg />} />
+          <Route path="/donorrole" exact element={<Donorrole />} />
+          <Route path="/teacherrole" exact element={<Teacherrole />} />
+          <Route path="/AdminOrgReq" exact element={<AdminOrgReq />} />
+          <Route path="/AdminDonReq" exact element={<AdminDonReq />} />
+          <Route path="/Adminchangepass" exact element={<Adminchangepass />} />
+          <Route path="/OrgReg" exact element={<OrgReg />} />
+          <Route path="/teacherprobono" exact element={<Teacherprobono />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
