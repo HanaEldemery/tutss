755f345336bc84372f42fe14ca95fec485794435
HALLATAREK <hallatarek@gmail.com>
2024-05-08 21:25:49 +0300

    Haloolaa bono bono

--- project/src/pages/teacherprobono.js
+++ project/src/pages/teacherprobono.js
@@ -1,56 +1,120 @@
 import React, { useState } from "react";
+import { Box, Checkbox, FormGroup, FormControlLabel, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
-import { Box, FormGroup, FormControlLabel, Checkbox, TextField } from "@mui/material"; // Import Material-UI components
-function teacherprobono() {
+
+function TeacherProbono() {
+  const [englishChecked, setEnglishChecked] = useState(false);
+  const [mathsChecked, setMathsChecked] = useState(false);
+  const [scienceChecked, setScienceChecked] = useState(false);
+  const [normalClasses, setNormalClasses] = useState(0);
+  const [privateTutoringClasses, setPrivateTutoringClasses] = useState(0);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+
+    if (!englishChecked && !mathsChecked && !scienceChecked) {
+      
+      setError("Please select at least one subject.");
+      const clearError = () => {
+        setError(null);
+      };
+      setTimeout(clearError, 5000);
+
+    }
+
+    else if (normalClasses === 0 ) {
+      setError("Please specify the number of classes.");
+      const clearError = () => {
+        setError(null);
+      };
+      setTimeout(clearError, 5000);
+    }
+    else if (privateTutoringClasses === 0) {
+      setError("Please specify the number of classes.");
+      const clearError = () => {
+        setError(null);
+      };
+      setTimeout(clearError, 5000);
+    }
+    else {
+      window.location.href = "/CommonPageteacher";
+
+    }
+
+  };
+ 
+
   return (
-<Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
-    }}
-    noValidate
-    autoComplete="off"
-  >
+    
+<div>
+
+
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
       <h1>ProBono Classes</h1>
-      <form id="contact-form" method="POST">
       <FormGroup>
-      <FormControlLabel control={<Checkbox defaultUnChecked />} label="English" />
-      <FormControlLabel control={<Checkbox defaultUnChecked />} label="Maths" />
-      <FormControlLabel control={<Checkbox defaultUnChecked />} label="Science" />  
-
-
-      <br />
-      <br />
-    </FormGroup>
-    <TextField
+        <FormControlLabel
+          control={<Checkbox checked={englishChecked} onChange={(e) => setEnglishChecked(e.target.checked)} />}
+          label="English"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={mathsChecked} onChange={(e) => setMathsChecked(e.target.checked)} />}
+          label="Maths"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={scienceChecked} onChange={(e) => setScienceChecked(e.target.checked)} />}
+          label="Science"
+        />
+        
+        <br />
+      </FormGroup>
+      <TextField
         id="normal-classes"
         label="# of normal classes"
         type="number"
-        inputProps={{ min: 0 }} // Prevents negative numbers
+        inputProps={{ min: 0 }}
         InputLabelProps={{
           shrink: true,
         }}
+        value={normalClasses}
+        onChange={(e) => setNormalClasses(e.target.value)}
       />
-            <br />
-            <br />  
-
+      <br />
+      <br />
       <TextField
         id="private-tutoring-classes"
         label="# of Private tutoring classes"
         type="number"
-        inputProps={{ min: 0 }} // Prevents negative numbers
+        inputProps={{ min: 0 }}
         InputLabelProps={{
           shrink: true,
         }}
+        value={privateTutoringClasses}
+        onChange={(e) => setPrivateTutoringClasses(e.target.value)}
+        
       />
-        <br />
-        <br />
-        <br />
-        <Link to="/CommonPageteacher">
-        <button>Submit</button>
-      </Link>
-  </form> 
-     </Box>
-  )
-}
-export default teacherprobono;
+      
+      <br />
+      <br />
+      <br />
+      
+          {error && <p>{error}</p>}
+        <button type="submit">Submit</button>
+      
+
+    </Box>
+    
+</div>
+  );
+
+      }
+export default TeacherProbono;
