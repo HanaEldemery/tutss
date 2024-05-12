1fa1b2f376146640c579063a097f4d0c7070c128
seifselims <124066228+seifselims@users.noreply.github.com>
2024-05-12 18:03:23 +0300

    background image

--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -2,11 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Home.css"; // Import the updated CSS file
 
+import backgroundImage from "../images/photo-1595561579181-263fe6175b73.avif"; // Import the background image
+
 function Home() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${backgroundImage})`, // Use the imported image
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px",
+  };
+
   return (
-    <div className="home">
+    <div className="home" style={backgroundImageStyle}>
       <div className="headerContainer">
-      <h1 style={{ fontSize: "75px", marginBottom: "40px", color: "#ffffff" }}>Who are you today?</h1>
+        <h1 style={{ fontSize: "75px", marginBottom: "40px", color: "#ffffff" }}>Who are you today?</h1>
 
         <Link to="/adminlogin">
           <button> I am an Admin</button>
